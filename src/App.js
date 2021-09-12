377722f588222c1b7391824af24575001ba8caf7
FTSyntrax <kapito929@gmail.com>
2021-09-12 09:53:28 +0300

    updating the app with a shop page, collection preview component and collection item component

--- src/App.js
+++ src/App.js
@@ -4,19 +4,22 @@ import { Switch, Route } from "react-router-dom";
 import "./App.css";
 
 import HomePage from "./pages/homepage/homepage.component";
+import ShopPage from "./pages/shop/shop.component";
 
-const HatsPage = () => (
-	<div>
-		<h1>Hats Page</h1>
-	</div>
-)
-
+//   fetch("https://makeup-api.herokuapp.com/api/v1/products.json")
+// 		.then((res) => {
+// 			return res.json();
+// 		})
+// 		.then((data) => {
+// 			console.log(data);
+// 		})
+// 		.catch((err) => console.log(err));
 function App() {
 	return (
 		<div>
 			<Switch>
 				<Route exact path="/" component={HomePage} />
-				<Route path="/hats" component={HatsPage} />
+				<Route path="/shop" component={ShopPage} />
 			</Switch>
 		</div>
 	);
