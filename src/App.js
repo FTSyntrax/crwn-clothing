edd7d367145ca1fe13b6540c5ad1849b0dc329bc
FTSyntrax <kapito929@gmail.com>
2022-01-30 22:07:10 +0200

    Added checkout page, checkout item component and updated cart to use selectors from reselect

--- src/App.js
+++ src/App.js
@@ -1,15 +1,20 @@
 import React from "react";
 import { Switch, Route, Redirect } from "react-router-dom";
 import { connect } from 'react-redux';
+import { createStructuredSelector } from "reselect";
 
 import "./App.css";
 
 import HomePage from "./pages/homepage/homepage.component";
 import ShopPage from "./pages/shop/shop.component";
 import SignInAndSignUpPage from "./pages/sign-in-and-sign-up/sign-in-and-sign-up.component";
+import CheckoutPage from "./pages/checkout/checkout.component";
+
 import Header from "./components/header/header.component";
+
 import { auth, createUserProfileDocument } from './firebase/firebase.utils';
-import { setCurrentUser } from './redux/user/user.actions';
+import { selectCurrentUser } from "./redux/user/user.selector";
+import { setCurrentUser } from "./redux/user/user.actions";
 //   fetch("https://makeup-api.herokuapp.com/api/v1/products.json")
 // 		.then((res) => {
 // 			return res.json();
@@ -50,20 +55,31 @@ class App extends React.Component {
 
 	render() {
 			return (
-			<div>
-				<Header />
-				<Switch>
-					<Route exact path="/" component={HomePage} />
-					<Route path="/shop" component={ShopPage} />
-					<Route exact path="/signin" render={() => this.props.currentUser ? (<Redirect to='/' />) : (<SignInAndSignUpPage />)} />
-				</Switch>
-			</div>
-		)
+				<div>
+					<Header />
+					<Switch>
+						<Route exact path="/" component={HomePage} />
+						<Route path="/shop" component={ShopPage} />
+						<Route exact path="/checkout" component={CheckoutPage} />
+						<Route
+							exact
+							path="/signin"
+							render={() =>
+								this.props.currentUser ? (
+									<Redirect to="/" />
+								) : (
+									<SignInAndSignUpPage />
+								)
+							}
+						/>
+					</Switch>
+				</div>
+			);
 	}
 }
 
-const mapStateToProps = ({ user }) => ({
-	currentUser: user.currentUser,
+const mapStateToProps = createStructuredSelector({
+	currentUser: selectCurrentUser,
 });
 
 const mapDispatchToProps = (dispatch) => ({
